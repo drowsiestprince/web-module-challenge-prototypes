664a86334f7161cc0739f2ce8ab5d661c3f63c43
James Montgomery <erenroseyeager@gmail.com>
2023-05-24 19:14:50 -0400

    Finished assignments

--- index.js
+++ index.js
@@ -15,10 +15,26 @@
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
-
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+  this.stomach = []
 }
 
+Person.prototype.toString = function () 
+{
+  return `${this.name}, ${this.age}`;
+};
+
+Person.prototype.eat = function (someFood)
+{
+  this.stomach.push(someFood);
+}
+
+Person.prototype.poop = function ()
+{
+  this.stomach = [];
+}
 
 /*
   TASK 2
@@ -36,8 +52,16 @@ function Person() {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
+function Car(model, milesPerGallon) {
+  this.model = model;
+  this.milesPerGallon = milesPerGallon;
+  this.tank = 0;
+  this.odometer = 0;
+}
 
+Car.prototype.fill = function (gallons)
+{
+  this.tank = this.tank + gallons;
 }
 
 
@@ -49,18 +73,26 @@ function Car() {
         + Should return a string "Playing with x", x being the favorite toy.
 */
 
-function Baby() {
+function Baby(name, age, favoriteToy) {
+  Person.call(this, name, age);
+  this.favoriteToy = favoriteToy; 
+}
 
+Baby.prototype = Object.create(Person.prototype);
+
+Baby.prototype.play = function ()
+{
+  return `Playing with ${this.favoriteToy}`;
 }
 
 
 /* 
   TASK 4
   In your own words explain the four principles for the "this" keyword below:
-  1. 
-  2. 
-  3. 
-  4. 
+  1. default binding
+  2. implicit binding
+  3. explicit binding
+  4. constructor call binding
 */
 
 ///////// END OF CHALLENGE /////////
